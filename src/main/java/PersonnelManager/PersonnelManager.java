c6dd5c3934cfcf6e82bd8a71593a5a224df64f67
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-02-15 09:17:14 -0500

    Testing

--- src/main/java/PersonnelManager/PersonnelManager.java
+++ src/main/java/PersonnelManager/PersonnelManager.java
@@ -12,11 +12,6 @@ import javafx.stage.Stage;
 public class PersonnelManager extends Application
 {
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage)
     {
