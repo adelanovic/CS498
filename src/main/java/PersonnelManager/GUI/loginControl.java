131fff9e8c3125eafde3f8bb41ae562bca37f8dd
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-02-17 21:12:01 -0500

    Finishing Login Page..

--- src/main/java/PersonnelManager/GUI/loginControl.java
+++ src/main/java/PersonnelManager/GUI/loginControl.java
@@ -3,6 +3,7 @@ package PersonnelManager.GUI;
 import PersonnelManager.MongoDriver;
 import com.mongodb.MongoCommandException;
 import javafx.event.Event;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -17,11 +18,23 @@ public class loginControl {
     @FXML public PasswordField passwordField;
 
 
+    //Method for controlling the Login Screen/Password verification
     @FXML private void passwordEntered(KeyEvent event) {
         try {
             if (event.getCode().equals(KeyCode.ENTER)) {
-                MongoDriver mongo_driver = new MongoDriver();
-                mongo_driver.connect_database();
+                if(passwordField.getText().isEmpty())
+                {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Password is required!");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Your password cannot be left blank.");
+                    alert.showAndWait();
+                }
+                else {
+
+                    MongoDriver mongo_driver = new MongoDriver();
+                    mongo_driver.connect_database();
+                }
             }
         }catch(MongoCommandException e){
             System.out.println("Incorrect Password or Username!");
