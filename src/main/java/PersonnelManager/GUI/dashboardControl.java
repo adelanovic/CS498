5536fa8dae59431085475ff99129f8ca6d3ad1ec
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-02-17 21:13:15 -0500

    Clean up

--- src/main/java/PersonnelManager/GUI/dashboardControl.java
+++ src/main/java/PersonnelManager/GUI/dashboardControl.java
@@ -1,22 +1,9 @@
 package PersonnelManager.GUI;
 
 import javafx.event.Event;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.MeshView;
-import javafx.scene.shape.TriangleMesh;
-import javafx.stage.FileChooser;
 import javafx.fxml.FXML;
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class dashboardControl {
    @FXML public AnchorPane loginPane;
