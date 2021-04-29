86a391d92d2f1b6810188a2e43c554d664bb3cf9
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-04-29 17:52:06 -0400

    Update initialstageLogin.java

--- src/main/java/com/application/initialstageLogin.java
+++ src/main/java/com/application/initialstageLogin.java
@@ -10,6 +10,15 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import java.io.IOException;
 
+/**
+ * Defines how the initial stage is set up and what stages are loaded first. Currently takes you to the login panes.
+ * Class: CS498 Capstone Project
+ * Instructor: Professor Spetka, Scott
+ * Date: 02/15/2021
+ * Last Revision: 02/15/2021
+ * @author Adis Delanovic
+ *
+ */
 
 public class initialstageLogin extends Application
 {
