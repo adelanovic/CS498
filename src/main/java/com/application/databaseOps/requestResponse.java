b89e7c833eb63c518b7dda4782454c42dc55d492
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-04-29 17:34:32 -0400

    Commented the requestResponse class
    Commented the requestResponse class

--- src/main/java/com/application/databaseOps/requestResponse.java
+++ src/main/java/com/application/databaseOps/requestResponse.java
@@ -2,6 +2,15 @@ package com.application.databaseOps;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Defines the Time Off request JSON to GSON Array.
+ * Class: CS498 Capstone Project
+ * Instructor: Professor Spetka, Scott
+ * Date: 02/15/2021
+ * Last Revision: 02/15/2021
+ * @author Adis Delanovic
+ *
+ */
 
 public class requestResponse {
     @SerializedName("user_id")
@@ -45,23 +54,47 @@ public class requestResponse {
     private String status;
 
 
-
+/**
+ * Returns the user ID of the current user that made the request
+ * @param none
+ * @return String, containing the user ID
+ */
     public String getUserId() {
         return userId;
     }
-
+    
+/**
+ * Returns the request end date of the current user that made the request
+ * @param none
+ * @return String, containing the request end date
+ */
     public String getRequest_end() {
         return request_end;
     }
-
+    
+/**
+ * Returns the request start date of the current user that made the request
+ * @param none
+ * @return String, containing the request start date
+ */
     public String getRequest_start(){
         return requestStart;
     }
-
+    
+/**
+ * Returns the request status of the current user that made the request
+ * @param none
+ * @return String, containing the status of the request (pending/approved)
+ */
     public String getRequest_Status(){
         return status;
     }
-
+    
+/**
+ * Returns the reason of the current user that made the request
+ * @param none
+ * @return String, containing the reason for the request
+ */
     public String getReason(){
         return reason;
     }
