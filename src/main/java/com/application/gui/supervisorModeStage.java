4b045a507bc5f613b9c473f541bb6513657997e5
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-04-28 22:36:00 -0400

    Added plenty of error handling and new features.

--- src/main/java/com/application/gui/supervisorModeStage.java
+++ src/main/java/com/application/gui/supervisorModeStage.java
@@ -1,9 +1,13 @@
 package com.application.gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class supervisorModeStage {
     @FXML public Button resourcesBtn;
@@ -11,20 +15,48 @@ public class supervisorModeStage {
     @FXML public Button requestsOffBtn;
     @FXML public Button schedulingBtn;
     @FXML public AnchorPane supervisorMainAnchor;
+    @FXML public AnchorPane resourcesMainPane;
+    @FXML public WebView supervisorWebView;
+    @FXML public WebEngine webEngine;
+    @FXML public AnchorPane passdownsAnchor;
+    @FXML public AnchorPane requestsAnchor;
+    @FXML public AnchorPane schedulingAnchor;
 
     public void displaySchedulePane(ActionEvent actionEvent) {
-        System.out.println("Test....");
+        resourcesMainPane.setVisible(false);
+        passdownsAnchor.setVisible(false);
+        schedulingAnchor.setVisible(true);
+        requestsAnchor.setVisible(false);
     }
 
     public void requestTimeOffPanee(ActionEvent actionEvent) {
-        System.out.println("Test....");
+        resourcesMainPane.setVisible(false);
+        passdownsAnchor.setVisible(false);
+        schedulingAnchor.setVisible(false);
+        requestsAnchor.setVisible(true);
     }
 
     public void displayDailyPassdownsPane(ActionEvent actionEvent) {
-        System.out.println("Test....");
+        passdownsAnchor.setVisible(true);
+        resourcesMainPane.setVisible(false);
+        schedulingAnchor.setVisible(false);
+        requestsAnchor.setVisible(false);
     }
 
     public void displayResourcesPane(ActionEvent actionEvent) {
-        System.out.println("Test....");
+        resourcesMainPane.setVisible(true);
+        passdownsAnchor.setVisible(false);
+        schedulingAnchor.setVisible(false);
+        requestsAnchor.setVisible(false);
+        webEngine = supervisorWebView.getEngine();
+        webEngine.load("https://www.e-publishing.af.mil/Product-Index/");
+
+
+        webEngine.getLoadWorker().exceptionProperty().addListener(new ChangeListener<Throwable>() {
+            @Override
+            public void changed(ObservableValue<? extends Throwable> observable, Throwable oldValue, Throwable newValue) {
+                System.out.println("Error Occured!");
+            }
+        });
     }
 }
