14ea58fac43c852f011122147e06e88400dacbe8
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-04-29 17:51:21 -0400

    Update launchApp.java

--- src/main/java/com/application/launchApp.java
+++ src/main/java/com/application/launchApp.java
@@ -2,6 +2,17 @@ package com.application;
 
 import javafx.application.Application;
 
+
+/**
+ * Launches the application and loads the required first stage set up.
+ * Class: CS498 Capstone Project
+ * Instructor: Professor Spetka, Scott
+ * Date: 02/15/2021
+ * Last Revision: 02/15/2021
+ * @author Adis Delanovic
+ *
+ */
+
 public class launchApp
 {
 
