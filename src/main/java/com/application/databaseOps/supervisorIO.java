18805e5f6abfa978452a66b03996574c27e77854
Adis Delanovic <42793838+adelanovic@users.noreply.github.com>
2021-04-29 17:50:22 -0400

    Update supervisorIO.java

--- src/main/java/com/application/databaseOps/supervisorIO.java
+++ src/main/java/com/application/databaseOps/supervisorIO.java
@@ -1,5 +1,14 @@
 package com.application.databaseOps;
 
+/**
+ * Defines the operations for the supervisor.
+ * Class: CS498 Capstone Project
+ * Instructor: Professor Spetka, Scott
+ * Date: 02/15/2021
+ * Last Revision: 02/15/2021
+ * @author Adis Delanovic
+ *
+ */
 public class supervisorIO extends employeeIO {
 
 }
